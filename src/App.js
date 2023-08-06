0cfb9536d44145d72b43eae21ed7509c696eeab3
FrauY <yuliayulina55omsk@gmail.com>
2023-08-06 19:30:30 +0200

    Added GitHub Repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1> Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://portfolio-yy.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Yulia Yulina
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/FrauY/homework-search-engine"
+          target="_ blank"
+          rel="noreferrer"
+        >
+          open-soursed on GitHab
+        </a>
+      </footer>
+    </div>
+  );
 }
